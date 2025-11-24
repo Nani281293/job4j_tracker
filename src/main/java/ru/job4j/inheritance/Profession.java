f1030e43f03e01f8de85a1547cc68543e2fbd38f
Ivan <Nani93@yandex.ru>
2025-11-24 20:37:49 +0300

    3. Вызов конструктора родителя super(...) [#6837 #555310]

--- src/main/java/ru/job4j/inheritance/Profession.java
+++ src/main/java/ru/job4j/inheritance/Profession.java
@@ -2,7 +2,8 @@ package ru.job4j.inheritance;
 
 public class Profession {
     private boolean degree;
-    public Profession (boolean degree) {
+
+    public Profession(boolean degree) {
         this.degree = degree;
     }
 }
