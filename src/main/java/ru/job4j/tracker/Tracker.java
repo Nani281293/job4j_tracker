4b76ac0cf4cea4ca9440fa115aabe2230784bff5
Ivan <Nani93@yandex.ru>
2025-11-27 23:01:48 +0300

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -56,4 +56,12 @@ public class Tracker {
         }
         return result;
     }
+
+    public void delete(int id) {
+        if (indexOf(id) != -1) {
+            System.arraycopy(items, indexOf(id) + 1, items, indexOf(id), size - indexOf(id) - 1);
+            items[size - 1] = null;
+            size--;
+        }
+    }
 }
