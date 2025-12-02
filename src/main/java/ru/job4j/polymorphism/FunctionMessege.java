92c6ded7d529b9e8c69708ad92dbab8bb17a3e8c
Ivan <Nani93@yandex.ru>
2025-12-02 21:36:34 +0300

    4.1. Элементы интерфейса [#504948]

---
+++ src/main/java/ru/job4j/polymorphism/FunctionMessege.java
@@ -0,0 +1,8 @@
+package ru.job4j.polymorphism;
+
+public class FunctionMessege implements FunctionTwo {
+    public static void main(String[] args) {
+        FunctionMessege functionMessege = new FunctionMessege();
+        functionMessege.functionMessage();
+    }
+}
